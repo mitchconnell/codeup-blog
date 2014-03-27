aed15f78491b5515a341181863fea2fd494dcb72
Mitch <mitchconnell@hotmail.com>
2014-03-27 11:06:31 -0500

    laravel
    laravel

---
+++ app/routes.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+// Route::get('/', function()
+// {
+//     return View::make('hello');
+// });
+
+// Route::get('/sayhello', function()
+// {
+//     return "Hello, Codeup!";
+// });
+
+// Route::get('/resume', function()
+// {
+// 	return "This is my resume";
+// });
+
+// Route::get('/portfolio', function()
+// {
+//     return "This is my portfolio";
+// });
+
+Route::get('/sayhello/{name}' ,function($name)
+{
+    return View::make('my-first-view');
+});
+
+
+
+
+
+
+
