00c53ba003121ec7219a39142dc4dba89b6227a9
Mitch <mitchconnell@hotmail.com>
2014-03-31 17:42:32 -0500

    update

--- app/models/Post.php
+++ app/models/Post.php
@@ -4,17 +4,15 @@ class Post extends BaseModel {
 
     protected $table = 'posts';
 
-}
-
-
-public static $rules = array(
-	'title'     => 'required|max:100',
-	'body'      => 'required|max:10000'
+	public static $rules = array(
+		'title'     => 'required|max:100',
+		'body'      => 'required|max:10000'
 
 
 	);
 
-public function setPasswordAttribute($value)
-{
-    $this->attributes['password'] = Hash::make($value);
-} 
+	public function setPasswordAttribute($value)
+	{
+	    $this->attributes['password'] = Hash::make($value);
+	}
+}
