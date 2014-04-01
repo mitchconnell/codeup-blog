e161229cb0386afa342a19bdfad9538c5e78cd00
Mitch <mitchconnell@hotmail.com>
2014-04-01 09:08:00 -0500

    Fixed error

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -2,18 +2,18 @@
 
 @section('content')
 	<h1>List Posts</h1>
-	@foreach ($posts as post)
 
+	@foreach ($posts as $post)
 	<p>
 		<a href="{{{action('PostsController@show', $post->id) }}}">{{{ $post->title }}}</a>
 		<p>{{ Str::words($post->body, 10) }}</p>
-		{{ $post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i A') }}
+		{{ $post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i:s A') }}
 	</p>
 	<hr>
 	@endforeach
 	{{ $posts->links() }}
 	<hr>
 	<p>
-		<a href="{{{ action('PostControllers@create') }}}">Create New Post</a>
+		<a href="{{{ action('PostsController@create') }}}">Create New Post</a>
 	</p>
 @stop
